d471e5410015e79ef7fc8ac60a2e1221901db953
Dongmin.yoon <ehdals8965@naver.com>
2019-11-03 08:35:08 +0900

    MapViewController 마커 추가

---
+++ CatchMe/CatchMe/Sources/Model/UserInform.swift
@@ -0,0 +1,15 @@
+//
+//  UserInform.swift
+//  CatchMe
+//
+//  Created by 윤동민 on 2019/11/03.
+//  Copyright © 2019 ns. All rights reserved.
+//
+
+import Foundation
+
+struct UserInform {
+    let userIdx: String
+    let lng: String
+    let lat: String
+}
